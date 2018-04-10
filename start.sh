0e3f0c7f2b4496bf42256e3347818c2b8066aa01
chenyingcai <cheningcai@outlook.com>
2018-04-10 19:43:47 +0200

    enrich the start.sh and make it more convenient

--- start.sh
+++ start.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+DOCKER_NAME="chenyingcai/resume:v1"
 RESUME_PATH="$PWD/resume"
 RESUME_PORT="$1"
 echo "Port: ${RESUME_PORT:="8080"}"
-
+if [ "$(docker images -q $DOCKER_NAME 2> /dev/null)" == "" ]; then
+  echo "没有找到$DOCKER_NAME容器"
+  echo "----------------------------------------------"
+  echo "开始创建c$DOCKER_NAME容器"
+  docker build -t $DOCKER_NAME .
+fi
 mkdir -p $RESUME_PATH
-docker run -d --name resume_tmp chenyingcai/resume:v1
+docker run -d --name resume_tmp $DOCKER_NAME
 if [ ! -d "$RESUME_PATH/config" ]; then
   docker cp resume_tmp:/usr/html/user/config $RESUME_PATH/config
 fi
@@ -20,10 +26,12 @@ docker run -d --name resume -p $RESUME_PORT:80 \
     -v $RESUME_PATH/pages:/usr/html/user/pages \
     -v $RESUME_PATH/config/:/usr/html/user/config/ \
     -v $RESUME_PATH/static/:/usr/html/static \
-    --restart=always chenyingcai/resume:v1
+    --restart=always $DOCKER_NAME
 echo "run the ngnix"
 docker exec -it resume run
 echo "Done"
 echo "generate the initial html file"
 docker exec -it resume generate
 echo "Done"
+echo "open the preview web"
+firefox -private "localhost:${RESUME_PORT:="8080"}"
